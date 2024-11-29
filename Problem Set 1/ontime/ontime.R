5b29d7780f8871f1c3ed896ec6fc301b534b5d26
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-29 21:57:04 +0530

    ok

--- Problem Set 1/ontime/ontime.R
+++ Problem Set 1/ontime/ontime.R
@@ -6,11 +6,10 @@ valid_routes <- c(bus$route, rail$route)
 
 repeat {
   # Prompt the user for a route
-  route <- readline(prompt = "Please enter the route you intend to take: ")
+  route <- readline(prompt = "Route: ")
   
   # Check if the route is valid
   if (route %in% valid_routes) {
-    cat("You have selected a valid route:", route, "\n")
     break  # Exit the loop if a valid route is entered
   } else {
     message("Invalid route. Please enter a valid route.")
