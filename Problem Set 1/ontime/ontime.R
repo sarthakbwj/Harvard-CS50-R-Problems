0daeaad2d6238360810a9024902ec1d062d1dddf
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-12-03 15:19:39 +0530

    Update ontime.R

--- Problem Set 1/ontime/ontime.R
+++ Problem Set 1/ontime/ontime.R
@@ -1,12 +1,22 @@
+library(dplyr)
+
 bus <- read.csv("/Users/sarthakbwj/Documents/GitHub/Harvard-CS50-R-Problems/Problem Set 1/ontime/bus.csv")
 rail <- read.csv("/Users/sarthakbwj/Documents/GitHub/Harvard-CS50-R-Problems/Problem Set 1/ontime/rail.csv")
 
 # Define valid routes
 valid_routes <- c(bus$route, rail$route)
+peak <- c()
+non_peak <- c()
 
 repeat {
   # Prompt the user for a route
   route <- readline(prompt = "Route: ")
+    if (peak %in% route){
+      return(peak)
+    }
+    else{
+      return(non_peak)
+    }
   
   # Check if the route is valid
   if (route %in% valid_routes) {
